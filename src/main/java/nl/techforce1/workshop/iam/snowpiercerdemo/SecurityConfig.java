dfce64a24175a632c487a8ead8b92964665f8e9d
Maarten Roelofswaard <Maarten.Roelofswaard@rabobank.nl>
2022-12-07 21:53:45 +0100

    Changed to stateless jwt impl.

--- src/main/java/nl/techforce1/workshop/iam/snowpiercerdemo/SecurityConfig.java
+++ src/main/java/nl/techforce1/workshop/iam/snowpiercerdemo/SecurityConfig.java
@@ -2,16 +2,15 @@ package nl.techforce1.workshop.iam.snowpiercerdemo;
 
 import static nl.techforce1.workshop.iam.snowpiercerdemo.domain.Role.DIRECTOR;
 import static nl.techforce1.workshop.iam.snowpiercerdemo.domain.Role.ENGINEER;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -25,23 +24,18 @@ class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-            .requestMatchers(HttpMethod.POST)
-            .hasRole(DIRECTOR.name())
-                .requestMatchers("snowpiercer/cars/engine/*")
-                .hasRole(ENGINEER.name())
-            .anyRequest()
-            .authenticated();
-        http.oauth2Login()
-            .and()
-            .logout()
-            .addLogoutHandler(keycloakLogoutHandler)
-            .logoutSuccessUrl("/");
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST)
+                        .hasRole(DIRECTOR.name())
+                        .requestMatchers("snowpiercer/cars/engine/*")
+                        .hasRole(ENGINEER.name())
+                        .anyRequest()
+                        .authenticated())
+                .oauth2ResourceServer(authConfigurer -> authConfigurer.jwt().jwkSetUri("http://localhost:8380/realms/snowpiercer/protocol/openid-connect/certs"))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults());
+
         return http.build();
     }
 
-    @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
 }
