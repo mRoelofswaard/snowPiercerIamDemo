d123c2b43254fcc0fbcc804650782a3474b2baea
Maarten Roelofswaard <Maarten.Roelofswaard@rabobank.nl>
2022-12-08 17:26:07 +0100

    Use the correct path match expressions for the endpoint security config.

--- src/main/java/nl/techforce1/workshop/iam/snowpiercerdemo/SecurityConfig.java
+++ src/main/java/nl/techforce1/workshop/iam/snowpiercerdemo/SecurityConfig.java
@@ -21,9 +21,9 @@ class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "snowpiercer/cars")
+        http.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/snowpiercer/cars")
                         .hasRole(DIRECTOR.name())
-                        .requestMatchers("snowpiercer/cars/engine/*")
+                        .requestMatchers("/snowpiercer/cars/engine/*")
                         .hasRole(ENGINEER.name())
                         .anyRequest()
                         .authenticated())
